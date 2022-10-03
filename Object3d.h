2cee8a8a5840726287a64530bfe8f158833e26a6
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-03 15:25:22 +0900

    CreateModelを書き換える

--- Object3d.h
+++ Object3d.h
@@ -37,10 +37,10 @@ public: // サブクラス
 	};
 
 private: // 定数
-	//static const int division = 50;					// 分割数
-	//static const float radius;				// 底面の半径
-	//static const float prizmHeight;			// 柱の高さ
-	//static const int planeCount = division * 2 + division * 2;		// 面の数
+	static const int division = 50;					// 分割数
+	static const float radius;				// 底面の半径
+	static const float prizmHeight;			// 柱の高さ
+	static const int planeCount = division * 2 + division * 2;		// 面の数
 	//static const int vertexCount = planeCount * 3;		// 頂点数
 	
 	static const int vertexCount = 4;		// 頂点数
