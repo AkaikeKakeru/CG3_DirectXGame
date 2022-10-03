20ae5df6bfaf879373b6f2fd0369cbb0dc8c132a
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-03 15:36:33 +0900

    WASDキーの処理を、カメラの視点のみ動かすようにする。

--- Object3d.cpp
+++ Object3d.cpp
@@ -130,6 +130,19 @@ void Object3d::CameraMoveVector(XMFLOAT3 move)
 	SetTarget(target_moved);
 }
 
+void Object3d::CameraMoveEyeVector(XMFLOAT3 move)
+{
+	XMFLOAT3 eye_moved = GetEye();
+	XMFLOAT3 target_moved = GetTarget();
+
+	eye_moved.x += move.x;
+	eye_moved.y += move.y;
+	eye_moved.z += move.z;
+
+	SetEye(eye_moved);
+}
+
+
 void Object3d::InitializeDescriptorHeap()
 {
 	HRESULT result = S_FALSE;
