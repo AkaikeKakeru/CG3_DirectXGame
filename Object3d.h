24f1920cc701a9a37ac9675e0e347d2ae4b928b2
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-03 15:10:28 +0900

    一旦3dオブジェクトを、円柱から四角形にしておく

--- Object3d.h
+++ Object3d.h
@@ -37,11 +37,14 @@ public: // サブクラス
 	};
 
 private: // 定数
-	static const int division = 50;					// 分割数
-	static const float radius;				// 底面の半径
-	static const float prizmHeight;			// 柱の高さ
-	static const int planeCount = division * 2 + division * 2;		// 面の数
-	static const int vertexCount = planeCount * 3;		// 頂点数
+	//static const int division = 50;					// 分割数
+	//static const float radius;				// 底面の半径
+	//static const float prizmHeight;			// 柱の高さ
+	//static const int planeCount = division * 2 + division * 2;		// 面の数
+	//static const int vertexCount = planeCount * 3;		// 頂点数
+	
+	static const int vertexCount = 4;		// 頂点数
+	static const int indexCount = 3 * 2;		// インデックス数
 
 public: // 静的メンバ関数
 	/// <summary>
@@ -136,10 +139,15 @@ private: // 静的メンバ変数
 	static D3D12_VERTEX_BUFFER_VIEW vbView;
 	// インデックスバッファビュー
 	static D3D12_INDEX_BUFFER_VIEW ibView;
+	//// 頂点データ配列
+	//static VertexPosNormalUv vertices[vertexCount];
+	//// 頂点インデックス配列
+	//static unsigned short indices[planeCount * 3];
+
 	// 頂点データ配列
 	static VertexPosNormalUv vertices[vertexCount];
 	// 頂点インデックス配列
-	static unsigned short indices[planeCount * 3];
+	static unsigned short indices[indexCount];
 
 private:// 静的メンバ関数
 	/// <summary>
