b5314c8f977a12a62745d91aadb76ad09b6ad6dd
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-31 16:12:15 +0900

    LineStrip

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,6 +1,6 @@
 #include "BasicShaderHeader.hlsli"
 
-[maxvertexcount(6)]
+[maxvertexcount(4)]
 void main(
 	triangle VSOutput input[3] : SV_POSITION,
 	inout LineStream< GSOutput > output
@@ -10,27 +10,16 @@ void main(
 
 	for (uint i = 0; i < 3; i++)
 	{
-		// 線分の始点
 		element.svpos = input[i].svpos;
 		element.normal = input[i].normal;
 		element.uv = input[i].uv;
+		// 頂点を1つ追加
 		output.Append(element);
-
-		// 線分の終点
-		if (i == 2) {
-			//+1すると溢れるので、最初に戻る
-			element.svpos = input[0].svpos;
-			element.normal = input[0].normal;
-			element.uv = input[0].uv;
-		}
-		else {
-			element.svpos = input[i + 1].svpos;
-			element.normal = input[i + 1].normal;
-			element.uv = input[i + 1].uv;
-		}
-		output.Append(element);
-
-		//現在のストリップを終了し、次のストリップを開始
-		output.RestartStrip();
 	}
+
+	// 最初の点をもう一度追加
+	element.svpos = input[0].svpos;
+	element.normal = input[0].normal;
+	element.uv = input[0].uv;
+	output.Append(element);
 }
