41b20395f65accf01a68b7c50c6a25c5e7352a03
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-31 15:34:10 +0900

    ジオメトリシェーダのHLSL読み込み処理を追加

--- Object3d.cpp
+++ Object3d.cpp
@@ -221,28 +221,28 @@ void Object3d::InitializeGraphicsPipeline()
 		exit(1);
 	}
 
-	//// ジオメトリシェーダの読み込みとコンパイル
-	//result = D3DCompileFromFile(
-	//	L"Resources/shaders/BasicGeometryShader.hlsl",	// シェーダファイル名
-	//	nullptr,
-	//	D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
-	//	"main", "gs_5_0",	// エントリーポイント名、シェーダーモデル指定
-	//	D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
-	//	0,
-	//	&gsBlob, &errorBlob);
-	//if (FAILED(result)) {
-	//	// errorBlobからエラー内容をstring型にコピー
-	//	std::string errstr;
-	//	errstr.resize(errorBlob->GetBufferSize());
+	// ジオメトリシェーダの読み込みとコンパイル
+	result = D3DCompileFromFile(
+		L"Resources/shaders/BasicGeometryShader.hlsl",	// シェーダファイル名
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
+		"main", "gs_5_0",	// エントリーポイント名、シェーダーモデル指定
+		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
+		0,
+		&gsBlob, &errorBlob);
+	if (FAILED(result)) {
+		// errorBlobからエラー内容をstring型にコピー
+		std::string errstr;
+		errstr.resize(errorBlob->GetBufferSize());
 
-	//	std::copy_n((char*)errorBlob->GetBufferPointer(),
-	//		errorBlob->GetBufferSize(),
-	//		errstr.begin());
-	//	errstr += "\n";
-	//	// エラー内容を出力ウィンドウに表示
-	//	OutputDebugStringA(errstr.c_str());
-	//	exit(1);
-	//}
+		std::copy_n((char*)errorBlob->GetBufferPointer(),
+			errorBlob->GetBufferSize(),
+			errstr.begin());
+		errstr += "\n";
+		// エラー内容を出力ウィンドウに表示
+		OutputDebugStringA(errstr.c_str());
+		exit(1);
+	}
 
 	// ピクセルシェーダの読み込みとコンパイル
 	result = D3DCompileFromFile(
