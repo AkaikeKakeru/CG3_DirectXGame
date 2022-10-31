2f6d89321adbed26bf9265747d004e18e1c33bb5
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-31 16:24:58 +0900

    プリミティブの複製

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,18 +1,34 @@
 #include "BasicShaderHeader.hlsli"
 
-[maxvertexcount(3)]
+[maxvertexcount(6)]
 void main(
 	triangle VSOutput input[3] : SV_POSITION,
 	inout TriangleStream< GSOutput > output
 )
 {
+	//1つ目の三角形
 	for (uint i = 0; i < 3; i++)
 	{
 		GSOutput element;
 		element.svpos = input[i].svpos;
 		element.normal = input[i].normal;
-		// uvを2倍に
-		element.uv = input[i].uv * 2.0f;
+		element.uv = input[i].uv;
+		output.Append(element);
+	}
+
+	//現在のストリップを終了
+	output.RestartStrip();
+
+	//1つ目の三角形
+	for (uint i = 0; i < 3; i++)
+	{
+		GSOutput element;
+
+		//X方向に20ずらす
+		element.svpos = input[i].svpos + float4(20.0f,0.0f,0.0f,0.0f);
+		element.normal = input[i].normal;
+		// uvを5倍に
+		element.uv = input[i].uv * 5.0f;
 		output.Append(element);
 	}
 }
