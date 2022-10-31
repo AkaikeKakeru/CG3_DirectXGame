6260eab6cffb90d99d789e124bd7748a5cd54236
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-31 15:57:41 +0900

    点の出力

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,17 +1,14 @@
 #include "BasicShaderHeader.hlsli"
 
-[maxvertexcount(3)]
+[maxvertexcount(1)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION,
+	point VSOutput input[1] : SV_POSITION,
 	inout PointStream< GSOutput > output
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
-		element.uv = input[i].uv;
-		output.Append(element);
-	}
+	GSOutput element;
+	element.svpos = input[0].svpos;
+	element.normal = input[0].normal;
+	element.uv = input[0].uv;
+	output.Append(element);
 }
