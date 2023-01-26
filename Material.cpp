01d8a05859358e1f0f85b0186cfc1297a5ed0a9a
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-26 15:40:40 +0900

    マテリアルクラスを用意

---
+++ Material.cpp
@@ -0,0 +1,147 @@
+#include "Material.h"
+#include <DirectXTex.h>
+#include <cassert>
+
+//省略
+template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+using namespace DirectX;
+
+//実体
+ComPtr<ID3D12Device> Material::device_ = nullptr;
+
+void Material::StaticInitialize(ID3D12Device* device) { Material::device_ = device; }
+
+Material* Material::Create() {
+	Material* instance = new Material;
+
+	instance->Initialize();
+
+	return instance;
+}
+void Material::Initialize() {
+	// 定数バッファの生成
+	CreateConstBuffer();
+}
+
+void Material::Update(){
+	// 定数バッファへデータ転送
+	constMap_->ambient = ambient_;
+	constMap_->diffuse = diffuse_;
+	constMap_->specular = specular_;
+	constMap_->alpha = alpha_;
+}
+
+void Material::CreateConstBuffer(){
+	HRESULT result;
+
+	// ヒーププロパティ
+	CD3DX12_HEAP_PROPERTIES heapPropsMaterial = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	// リソース設定
+	CD3DX12_RESOURCE_DESC resourceDescMaterial = CD3DX12_RESOURCE_DESC::Buffer((sizeof(ConstBufferDataMaterial) + 0xff) & ~0xff);
+
+	// 定数バッファの生成
+	result = device_->CreateCommittedResource(
+		&heapPropsMaterial, // アップロード可能
+		D3D12_HEAP_FLAG_NONE,
+		&resourceDescMaterial,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&constBuff_));
+	assert(SUCCEEDED(result));
+
+	// 定数バッファへデータ転送
+
+	result = constBuff_->Map(0, nullptr, (void**)&constMap_);
+	//if (SUCCEEDED(result)) {
+	//	constMap1->ambient = material_.ambient;
+	//	constMap1->diffuse = material_.diffuse;
+	//	constMap1->specular = material_.specular;
+	//	constMap1->alpha = material_.alpha;
+	//	constBuffMaterial_->Unmap(0, nullptr);
+	//}
+}
+
+void Material::LoadTexture(const std::string& directoryPath,
+	CD3DX12_CPU_DESCRIPTOR_HANDLE cpuHandle,
+	CD3DX12_GPU_DESCRIPTOR_HANDLE gpuHandle) {
+	HRESULT result = S_FALSE;
+
+	TexMetadata metadata{};
+	ScratchImage scratchImg{};
+
+	//ファイルパスを結合
+	std::string filepath = directoryPath + filename_;
+
+	//ユニコード文字列に変換する
+	wchar_t wfilepath[128];
+	int iBufferSize = MultiByteToWideChar(
+		CP_ACP, 0,
+		filepath.c_str(), -1, wfilepath,
+		_countof(wfilepath));
+
+	result = LoadFromWICFile(
+		wfilepath,
+		WIC_FLAGS_NONE,
+		&metadata,
+		scratchImg);
+
+	ScratchImage mipChain{};
+	// ミップマップ生成
+	result = GenerateMipMaps(
+		scratchImg.GetImages(), scratchImg.GetImageCount(), scratchImg.GetMetadata(),
+		TEX_FILTER_DEFAULT, 0, mipChain);
+	if (SUCCEEDED(result)) {
+		scratchImg = std::move(mipChain);
+		metadata = scratchImg.GetMetadata();
+	}
+
+	// 読み込んだディフューズテクスチャをSRGBとして扱う
+	metadata.format = MakeSRGB(metadata.format);
+
+	// リソース設定
+	CD3DX12_RESOURCE_DESC texresDesc = CD3DX12_RESOURCE_DESC::Tex2D(
+		metadata.format, metadata.width, (UINT)metadata.height, (UINT16)metadata.arraySize,
+		(UINT16)metadata.mipLevels);
+
+	// ヒーププロパティ
+	CD3DX12_HEAP_PROPERTIES heapProps =
+		CD3DX12_HEAP_PROPERTIES(D3D12_CPU_PAGE_PROPERTY_WRITE_BACK, D3D12_MEMORY_POOL_L0);
+
+	// テクスチャ用バッファの生成
+	result = device_->CreateCommittedResource(
+		&heapProps, D3D12_HEAP_FLAG_NONE, &texresDesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ, // テクスチャ用指定
+		nullptr, IID_PPV_ARGS(&texbuff_));
+	assert(SUCCEEDED(result));
+
+	// テクスチャバッファにデータ転送
+	for (size_t i = 0; i < metadata.mipLevels; i++) {
+		const Image* img = scratchImg.GetImage(i, 0, 0); // 生データ抽出
+		result = texbuff_->WriteToSubresource(
+			(UINT)i,
+			nullptr,              // 全領域へコピー
+			img->pixels,          // 元データアドレス
+			(UINT)img->rowPitch,  // 1ラインサイズ
+			(UINT)img->slicePitch // 1枚サイズ
+		);
+		assert(SUCCEEDED(result));
+	}
+
+	// シェーダリソースビュー作成
+	cpuDescHandleSRV_ = cpuHandle;
+	gpuDescHandleSRV_ = gpuHandle;
+
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{}; // 設定構造体
+	D3D12_RESOURCE_DESC resDesc = texbuff_->GetDesc();
+
+	srvDesc.Format = resDesc.Format;
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
+	srvDesc.Texture2D.MipLevels = 1;
+
+	device_->CreateShaderResourceView(texbuff_.Get(), //ビューと関連付けるバッファ
+		&srvDesc, //テクスチャ設定情報
+		cpuDescHandleSRV_
+	);
+}
