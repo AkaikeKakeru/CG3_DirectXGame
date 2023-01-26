4473fe147a1f70dfb59403036264d2d28857454e
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-26 23:10:13 +0900

    サブクラス追加

--- Light.h
+++ Light.h
@@ -1,5 +1,14 @@
 #pragma once
+#include "Vector3.h"
 
 class Light {
+private: // エイリアス
+	//省略
+	template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 
+public://サブクラス
+	struct ConstBufferData {
+		Vector3 lightv; //ライトへの方向ベクトル
+		Vector3 lightcolor;//ライトの色
+	};
 };
