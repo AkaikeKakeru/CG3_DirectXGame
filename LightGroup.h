94233566f0df9009c181b00e306d145bb488f9b0
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-02-15 13:39:41 +0900

    サブクラスを追加

--- LightGroup.h
+++ LightGroup.h
@@ -10,8 +10,18 @@ private: // エイリアス
 	template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 
 public://定数
+	static const int DirLightNum_ = 3;
 
 public://サブクラス
+	//定数バッファ用データ構造体
+	struct ConstBufferDat {
+		//環境光の色
+		Vector3 ambientColor_;
+		float pad1_;
+		//平行光源用
+		DirectionalLight::ConstBufferData dirLights_[DirLightNum_];
+	};
+
 
 public://静的メンバ関数
 
